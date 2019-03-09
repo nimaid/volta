c2a596cacd44aa3ba51e13105292dc6e12e4ce64
Ella Jameson <ellagjameson@gmail.com>
2019-03-08 18:41:34 -0700

    add cuda 9 install script

---
+++ install_cuda9.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [ ! -f cuda_9.0.176_384.81_linux-run ]; then
+    wget https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda_9.0.176_384.81_linux-run -q --show-progress
+fi
+sudo chmod +x cuda_9.0.176_384.81_linux-run
+sudo ./cuda_9.0.176_384.81_linux-run -override --silent -toolkit -driver
+
+if [ ! -f cuda_9.0.176.1_linux-run ]; then
+    wget https://developer.nvidia.com/compute/cuda/9.0/Prod/patches/1/cuda_9.0.176.1_linux-run -q --show-progress
+fi
+sudo chmod +x cuda_9.0.176.1_linux-run
+sudo ./cuda_9.0.176.1_linux-run --silent -accept-eula
+
+if [ ! -f cuda_9.0.176.2_linux-run ]; then
+    wget https://developer.nvidia.com/compute/cuda/9.0/Prod/patches/2/cuda_9.0.176.2_linux-run -q --show-progress
+fi
+sudo chmod +x cuda_9.0.176.2_linux-run
+sudo ./cuda_9.0.176.2_linux-run --silent -accept-eula
+
+if [ ! -f cuda_9.0.176.3_linux-run ]; then
+    wget https://developer.nvidia.com/compute/cuda/9.0/Prod/patches/3/cuda_9.0.176.3_linux-run -q --show-progress
+fi
+sudo chmod +x cuda_9.0.176.3_linux-run
+sudo ./cuda_9.0.176.3_linux-run --silent -accept-eula
+
+if [ ! -f cuda_9.0.176.4_linux-run ]; then
+    wget https://developer.nvidia.com/compute/cuda/9.0/Prod/patches/4/cuda_9.0.176.4_linux-run -q --show-progress
+fi
+sudo chmod +x cuda_9.0.176.4_linux-run
+sudo ./cuda_9.0.176.4_linux-run --silent -accept-eula
+
+sudo apt-get install nvidia-cuda-toolkit -qq -y
+
+#PATH=$PATH:/usr/local/cuda-9.0/bin
