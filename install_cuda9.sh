8fa3c51f84003ee84976b4325110578a7ff501a8
Ella Jameson <ellagjameson@gmail.com>
2019-03-08 19:38:56 -0700

    add bash

--- install_cuda9.sh
+++ install_cuda9.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
+: '
 echo Installing CUDA 9.0 Runfile...
 if [ ! -f cuda_9.0.176_384.81_linux-run ]; then
     wget https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda_9.0.176_384.81_linux-run -q --show-progress
 fi
 sudo chmod +x cuda_9.0.176_384.81_linux-run
-sudo ./cuda_9.0.176_384.81_linux-run -override --silent -toolkit -driver
+sudo bash ./cuda_9.0.176_384.81_linux-run -override --silent -toolkit -driver
 sudo rm cuda_9.0.176_384.81_linux-run
 
 echo Installing CUDA Toolkit...
@@ -16,7 +16,7 @@ if [ ! -f cuda_9.0.176.1_linux-run ]; then
     wget https://developer.nvidia.com/compute/cuda/9.0/Prod/patches/1/cuda_9.0.176.1_linux-run -q --show-progress
 fi
 sudo chmod +x cuda_9.0.176.1_linux-run
-sudo ./cuda_9.0.176.1_linux-run --silent -accept-eula
+sudo bash ./cuda_9.0.176.1_linux-run --silent -accept-eula
 sudo rm cuda_9.0.176.1_linux-run
 
 echo Installing CUDA 9.0 Patch 2...
@@ -24,7 +24,7 @@ if [ ! -f cuda_9.0.176.2_linux-run ]; then
     wget https://developer.nvidia.com/compute/cuda/9.0/Prod/patches/2/cuda_9.0.176.2_linux-run -q --show-progress
 fi
 sudo chmod +x cuda_9.0.176.2_linux-run
-sudo ./cuda_9.0.176.2_linux-run --silent -accept-eula
+sudo bash ./cuda_9.0.176.2_linux-run --silent -accept-eula
 sudo rm cuda_9.0.176.2_linux-run
 
 echo Installing CUDA 9.0 Patch 3...
@@ -32,15 +32,16 @@ if [ ! -f cuda_9.0.176.3_linux-run ]; then
     wget https://developer.nvidia.com/compute/cuda/9.0/Prod/patches/3/cuda_9.0.176.3_linux-run -q --show-progress
 fi
 sudo chmod +x cuda_9.0.176.3_linux-run
-sudo ./cuda_9.0.176.3_linux-run --silent -accept-eula
+sudo bash ./cuda_9.0.176.3_linux-run --silent -accept-eula
 sudo rm cuda_9.0.176.3_linux-run
+'
 
 echo Installing CUDA 9.0 Patch 4...
 if [ ! -f cuda_9.0.176.4_linux-run ]; then
     wget https://developer.nvidia.com/compute/cuda/9.0/Prod/patches/4/cuda_9.0.176.4_linux-run -q --show-progress
 fi
 sudo chmod +x cuda_9.0.176.4_linux-run
-sudo ./cuda_9.0.176.4_linux-run --silent -accept-eula
+sudo bash ./cuda_9.0.176.4_linux-run --silent -accept-eula
 sudo rm cuda_9.0.176.4_linux-run
 
 #PATH=$PATH:/usr/local/cuda-9.0/bin
