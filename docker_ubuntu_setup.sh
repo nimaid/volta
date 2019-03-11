187d87fb02167ea1a04e26b01a362c56b46bb631
nimaid <ellagjameson@gmail.com>
2019-03-10 21:52:22 -0700

    whoops

--- docker_ubuntu_setup.sh
+++ docker_ubuntu_setup.sh
@@ -76,5 +76,5 @@ cd volta
 wget -O starry_night.jpg https://www.farmersalmanac.com/wp-content/uploads/2015/02/Starry-Night-Van-Gogh-Which-Stars-GoogleArtProject.jpg -q --show-status
 wget -O city.jpg https://upload.wikimedia.org/wikipedia/commons/a/a4/Toru%C5%84%2C_Szeroka_Street_%28DerHexer%29_2010-07-17_053.jpg -q --show-status
 
-./volta-1x.sh starry_night.jpg city.jpg
+./volta-x1.sh starry_night.jpg city.jpg
 
