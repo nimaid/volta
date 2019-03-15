e5b207511496012f88db90a519ccf840267b7ae0
nimaid <ellagjameson@gmail.com>
2019-03-15 12:04:58 -0700

    Dockerfile updates for Jupyter

--- GCE_setup.sh
+++ GCE_setup.sh
@@ -2,6 +2,8 @@
 # I started with a pre-made deep learning image because I was having issues getting nvidia-docker to talk with the drivers on Ubuntu 16.04
 # "Intel® optimized Deep Learning Image: Base m21 (with Intel® MKL and CUDA 10.0)"
 
+#curl -O -s https://gist.githubusercontent.com/durgeshm/b149e7baec4d4508eb4b2914d63018c7/raw/798aadbb54b451abcaba9bfeb833327fa4b3d53b/deps_nvidia_docker.sh -- may automate from 16.04 --> nvidia-docker
+
 # Install docker
 wget -O - -q 'https://gist.githubusercontent.com/allenday/c875eaf21a2b416f6478c0a48e428f6a/raw/f7feca1acc1a992afa84f347394fd7e4bfac2599/install-docker-ce.sh' | sudo bash
 
@@ -14,3 +16,5 @@ sudo docker pull nimaid/volta-deep-style:test1
 
 # Reboot and you're ready to go!
 sudo reboot
+
+# Run with "sudo docker run --runtime=nvidia -it -p 8888:8888 nimaid/volta-deep-style:test1
